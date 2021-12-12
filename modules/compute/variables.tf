7ef67075e91ddfd99b8ad8a3f9ad6109bc2af4a0
PaulKrznaric <paulkrznaric@gmail.com>
2021-12-11 23:02:10 -0500

    Initial commit

---
+++ modules/compute/variables.tf
@@ -0,0 +1,23 @@
+#-----compute/variables.tf-----
+#===============================
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "ssh_key_public" {
+  type    = string
+  #Replace this with the location of you public key .pub
+  default = "~/documents/school/key2.pub"
+}
+
+variable "ssh_key_private" {
+  type    = string
+  #Replace this with the location of you private key
+  default = "~/documents/school/key2"
+}
+variable "subnet_ips" {}
+
+variable "security_group" {}
+
+variable "subnets" {}
